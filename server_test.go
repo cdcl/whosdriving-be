5c2c54b6086496c4419f81a859d605c41114fec7
Carl Declercq <declercq.carl@gmail.com>
2022-08-28 19:18:03 +0000

    Go for graphql-api with gqlgen

--- server_test.go
+++ server_test.go
@@ -1,22 +1,96 @@
 package main
 
 import (
-	"net/http"
-	"net/http/httptest"
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestSetupRoutes(t *testing.T) {
-	SetupRoutes()
+func TestNewDb(t *testing.T) {
+	os.Remove("./test_new_db.sqlite3")
 
-	w := httptest.NewRecorder()
+	db := newDb("./test_new_db.sqlite3")
+	defer db.Close()
 
-	req, _ := http.NewRequest("GET", "/getUserData", nil)
+	_, err := db.Exec("create table foo (id integer not null primary key, name text);")
+	if err != nil {
+		t.Fatal(err)
+	}
 
-	getUserData(w, req)
+	tx, err := db.Begin()
+	if err != nil {
+		t.Fatal(err)
+	}
 
-	assert.Equal(t, 200, w.Code)
-	assert.Equal(t, "{\"Email\":\"toto@gmail.com\",\"Name\":\"Frank\"}", w.Body.String())
+	stmt, err := tx.Prepare("INSERT INTO foo(id, name) values(?, ?)")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer stmt.Close()
+
+	var expected = [...]string{"Fatima", "Robert", "Lisa", "Ahmed", "Inconnu"}
+
+	for i, n := range expected {
+		_, err = stmt.Exec(i, n)
+		if err != nil {
+			t.Fatalf("on row %d : %s", i, err)
+		}
+	}
+
+	err = tx.Commit()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rows, err := db.Query("select id, name from foo order by id desc")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var id int
+		var name string
+		err = rows.Scan(&id, &name)
+		if err != nil {
+			t.Fatal(err)
+		}
+		t.Logf("%d|%s", id, name)
+		assert.EqualValues(t, expected[id], name)
+	}
+
+	err = rows.Err()
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestCustomConfig(t *testing.T) {
+	var expectedConfig Config
+	expectedConfig.host = "BBBB"
+	expectedConfig.port = "AAAA"
+	expectedConfig.dbPath = "CCCC"
+
+	os.Setenv("HOST", expectedConfig.host)
+	os.Setenv("PORT", expectedConfig.port)
+	os.Setenv("DB_PATH", expectedConfig.dbPath)
+
+	config := createConfig()
+	t.Log(config)
+	assert.EqualValues(t, config.host, expectedConfig.host)
+	assert.EqualValues(t, config.port, expectedConfig.port)
+	assert.EqualValues(t, config.dbPath, expectedConfig.dbPath)
+}
+
+func TestDefaultConfig(t *testing.T) {
+	os.Unsetenv("HOST")
+	os.Unsetenv("PORT")
+	os.Unsetenv("DB_PATH")
+
+	config := createConfig()
+	t.Log(config)
+	assert.EqualValues(t, config.host, defaultHost)
+	assert.EqualValues(t, config.port, defaultPort)
+	assert.EqualValues(t, config.dbPath, defaultDbHostPath)
 }

4cf3bc17223e786bc017def48f949eeeabbadd96
Carl Declercq <declercq.carl@gmail.com>
2022-08-28 19:18:03 +0000

    Go for graphql-api with gqlgen

--- server.go
+++ server.go
@@ -1,34 +1,78 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
+	"database/sql"
 	"log"
 	"net/http"
+	"os"
+
+	_ "github.com/mattn/go-sqlite3"
+
+	"whosdriving-be/graph"
+	"whosdriving-be/graph/generated"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
 )
 
-type UserData struct {
-	Email string
-	Name  string
+const defaultHost = "127.0.0.1"
+const defaultPort = "8080"
+const defaultDbHostPath = "/app/data/whosdriving"
+
+type Config struct {
+	host   string
+	port   string
+	dbPath string
 }
 
-func getUserData(w http.ResponseWriter, r *http.Request) {
-	userData := &UserData{Email: "toto@gmail.com", Name: "Frank"}
-	userDataStr, err := json.Marshal(userData)
-	if err != nil {
-		fmt.Fprintf(w, "{Error:%s}", err)
-		return
+func createConfig() Config {
+	var config Config
+	var found bool
+	config.host, found = os.LookupEnv("HOST")
+	if !found {
+		config.host = defaultHost
 	}
-	fmt.Fprintf(w, string(userDataStr))
+
+	config.port, found = os.LookupEnv("PORT")
+	if !found {
+		config.port = defaultPort
+	}
+
+	config.dbPath, found = os.LookupEnv("DB_PATH")
+	if !found {
+		config.dbPath = defaultDbHostPath
+	}
+	return config
 }
 
-func SetupRoutes() {
-	http.HandleFunc("/getUserData", getUserData)
+func newDb(dbPath string) *sql.DB {
+	db, dbErr := sql.Open("sqlite3", dbPath)
+	if dbErr != nil {
+		db.Close()
+		log.Fatal(dbErr)
+	}
+
+	// and validate dsn
+	pingErr := db.Ping()
+	if pingErr != nil {
+		db.Close()
+		log.Fatal(pingErr)
+	}
+
+	return db
 }
 
 func main() {
-	portNumber := "9000"
-	SetupRoutes()
-	fmt.Println("Server listening on port ", portNumber)
-	log.Fatal(http.ListenAndServe(":"+portNumber, nil))
+
+	config := createConfig()
+	db := newDb(config.dbPath)
+	defer db.Close()
+
+	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{DB: db}}))
+
+	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	http.Handle("/query", srv)
+
+	log.Printf("connect to http://%s:%s/ for GraphQL playground", config.host, config.port)
+	log.Fatal(http.ListenAndServe(config.host+":"+config.port, nil))
 }
